78211c34dd50317380479c07ac2a3d90cffc125c
ningxiaobei <ningxiaobei@wandoujia.com>
2014-12-19 15:02:45 +0800

    首页专题部分

--- app/protected/views/index/index.php
+++ app/protected/views/index/index.php
@@ -1,22 +1,5 @@
 
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="UTF-8">
-    <title>SnapTube - Download YouTube Videos and Music</title>
-    <meta name="description" content="Download YouTube videos to your Android. It’s easy, fast, and free. Get the official version here.">
-    <meta name="keywords" content="android, youtube, download">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta name="twitter:card" content="summary_large_image">
-    <meta name="twitter:image:src" content="http://www.snappea.com/images/snaptube.50ebd0c0.png">
-    <meta property="og:image" content="http://www.snappea.com/images/snaptube.50ebd0c0.png">
-    <meta itemprop="image" content="http://www.snappea.com/images/snaptube.50ebd0c0.png">
-    <link rel="shortcut icon" href="images/favicon2.ico" type="image/x-icon">
-    <link rel="icon" href="images/favicon2.ico" type="image/x-icon">
-    <link rel="stylesheet" href="css/snaptube.ae38e891.css"/>
-    <script>window.html5 = { 'shivCSS': false };</script>
-  </head>
-  <body class="index">
+  
     <header><a href="//www.snaptubevideo.com/" class="logo">SnapTube</a>
       <nav><a href="https://www.facebook.com/pages/SnapTube/746438775443012" onclick="ga('send', 'event', 'snaptubevideo.com', 'snaptube', 'facebook');" class="facebook">Facebook</a><a href="https://twitter.com/snaptubeapp" onclick="ga('send', 'event', 'snaptubevideo.com', 'snaptube', 'twitter');" class="twitter">Twitter</a></nav>
     </header>
@@ -98,13 +81,11 @@
     <section id="categories" class="categories">
       <h3>Categories</h3>
       <ul>
-        <li><a href="#">Music</a></li>
-        <li><a href="#">Entertainment</a></li>
-        <li><a href="#">Games</a></li>
-        <li><a href="#">Movies</a></li>
-        <li><a href="#">Lifestylec</a></li>
-        <li><a href="#">News</a></li>
-        <li><a href="#">Educations</a></li>
+        <?php if(count($categories)){
+                foreach ($categories as $item) {
+        ?>   
+        <li><a href="#"><?php echo $item['alias']; ?></a></li>       
+        <?php }}?>
       </ul>
     </section>
     <!-- categories [e] -->
@@ -114,113 +95,32 @@
     <article id="recommended">
     <div class="recommended">
       <h3>Recommended Playlists</h3>
+      <?php if(count($specials)) {
+          foreach ($specials as $special) {
+      ?>
       <div class="hd">
-        <h4><a href="#">Movie Soundtrack Hit's 14</a></h4>
+        <h4><a href="#"><?php echo $special['special']['name']; ?></a></h4>
         <a href="#" class="more">More>></a>
       </div>
       <div class="bd">
         <ul>
-        <li>
-          <figure class="pic">
-            <a href="#"><img src="images/recommended_1.jpg" alt="recommended" />
-            <span class="time">2:47</span>
-            <span class="overlay"></span></a>
-          </figure>
-          <p class="title"><a href="#">Alone Offical Theatrical Trailer | Bipasha Basu,Karan Singh...</a></p>
-          <p class="info">3,681,223 views6 dats ago</p>
-        </li>
-        <li>
-          <figure class="pic">
-            <a href="#"><img src="images/recommended_2.jpg" alt="recommended" />
-            <span class="time">2:47</span>
-            <span class="overlay"></span></a>
-          </figure>
-          <p class="title"><a href="#">Alone Offical Theatrical Trailer | Bipasha Basu,Karan Singh...</a></p>
-          <p class="info">3,681,223 views6 dats ago</p>
-        </li>
-        <li>
-          <figure class="pic">
-            <a href="#"><img src="images/recommended_2.jpg" alt="recommended" />
-            <span class="time">2:47</span>
-            <span class="overlay"></span></a>
-          </figure>
-          <p class="title"><a href="#">Alone Offical Theatrical Trailer | Bipasha Basu,Karan Singh...</a></p>
-          <p class="info">3,681,223 views6 dats ago</p>
-        </li>
+          <?php if(count($special['items'])) {
+              foreach ($special['items'] as $item) {
+          ?>
+          <li>
+            <figure class="pic">
+              <a href="#"><img src="<?php echo $item['coverRes']; ?>" alt="recommended" width="196" height="110"/>
+              <span class="time">2:47</span>
+              <span class="overlay"></span></a>
+            </figure>
+            <p class="title"><a href="#"><?php echo $item['title']; ?></a></p>
+            <p class="info"><?php echo number_format($item['playCount']); ?> views <?php echo $this->formatDate($item['createdDate']); ?></p>
+          </li> 
+          <?php }}?>
         </ul>
-      </div>
-
-      <div class="hd">
-        <h4><a href="#">New music this week</a></h4>
-        <a href="#" class="more">More>></a>
-      </div>
-      <div class="bd">
-        <ul>
-        <li>
-          <figure class="pic">
-            <a href="#"><img src="images/recommended_1.jpg" alt="recommended" />
-            <span class="time">2:47</span>
-            <span class="overlay"></span></a>
-          </figure>
-          <p class="title"><a href="#">Alone Offical Theatrical Trailer | Bipasha Basu,Karan Singh...</a></p>
-          <p class="info">3,681,223 views6 dats ago</p>
-        </li>
-        <li>
-          <figure class="pic">
-            <a href="#"><img src="images/recommended_2.jpg" alt="recommended" />
-            <span class="time">2:47</span>
-            <span class="overlay"></span></a>
-          </figure>
-          <p class="title"><a href="#">Alone Offical Theatrical Trailer | Bipasha Basu,Karan Singh...</a></p>
-          <p class="info">3,681,223 views6 dats ago</p>
-        </li>
-        <li>
-          <figure class="pic">
-            <a href="#"><img src="images/recommended_2.jpg" alt="recommended" />
-            <span class="time">2:47</span>
-            <span class="overlay"></span></a>
-          </figure>
-          <p class="title"><a href="#">Alone Offical Theatrical Trailer | Bipasha Basu,Karan Singh...</a></p>
-          <p class="info">3,681,223 views6 dats ago</p>
-        </li>
-        </ul>
-      </div>
-
-      <div class="hd">
-        <h4><a href="#">Hot New Trailers</a></h4>
-        <a href="#" class="more">More>></a>
-      </div>
-      <div class="bd">
-        <ul>
-        <li>
-          <figure class="pic">
-            <a href="#"><img src="images/recommended_1.jpg" alt="recommended" />
-            <span class="time">2:47</span>
-            <span class="overlay"></span></a>
-          </figure>
-          <p class="title"><a href="#">Alone Offical Theatrical Trailer | Bipasha Basu,Karan Singh...</a></p>
-          <p class="info">3,681,223 views6 dats ago</p>
-        </li>
-        <li>
-          <figure class="pic">
-            <a href="#"><img src="images/recommended_2.jpg" alt="recommended" />
-            <span class="time">2:47</span>
-            <span class="overlay"></span></a>
-          </figure>
-          <p class="title"><a href="#">Alone Offical Theatrical Trailer | Bipasha Basu,Karan Singh...</a></p>
-          <p class="info">3,681,223 views6 dats ago</p>
-        </li>
-        <li>
-          <figure class="pic">
-            <a href="#"><img src="images/recommended_2.jpg" alt="recommended" />
-            <span class="time">2:47</span>
-            <span class="overlay"></span></a>
-          </figure>
-          <p class="title"><a href="#">Alone Offical Theatrical Trailer | Bipasha Basu,Karan Singh...</a></p>
-          <p class="info">3,681,223 views6 dats ago</p>
-        </li>
-        </ul>
-      </div>
+      </div> 
+      <?php }} ?> 
+      
       <div class="more-list"><a href="#">More lists>></a></div>
     </div>
     </article>
@@ -233,7 +133,7 @@
       <ul>
       <li>
         <figure class="pic">
-          <a href="#"><img src="images/weekly_1.jpg" alt="top_weekly" />
+          <a href="#"><img src="/static/images/weekly_1.jpg" alt="top_weekly" />
           <span class="time">2:47</span>
           <span class="overlay"></span></a>
         </figure>
@@ -244,7 +144,7 @@
       </li>
       <li>
         <figure class="pic">
-          <a href="#"><img src="images/weekly_1.jpg" alt="top_weekly" />
+          <a href="#"><img src="/static/images/weekly_1.jpg" alt="top_weekly" />
           <span class="time">2:47</span>
           <span class="overlay"></span></a>
         </figure>
@@ -255,7 +155,7 @@
       </li>
       <li>
         <figure class="pic">
-          <a href="#"><img src="images/weekly_1.jpg" alt="top_weekly" />
+          <a href="#"><img src="/static/images/weekly_1.jpg" alt="top_weekly" />
           <span class="time">2:47</span>
           <span class="overlay"></span></a>
         </figure>
@@ -266,7 +166,7 @@
       </li>
       <li>
         <figure class="pic">
-          <a href="#"><img src="images/weekly_1.jpg" alt="top_weekly" />
+          <a href="#"><img src="/static/images/weekly_1.jpg" alt="top_weekly" />
           <span class="time">2:47</span>
           <span class="overlay"></span></a>
         </figure>
@@ -277,7 +177,7 @@
       </li>
       <li>
         <figure class="pic">
-          <a href="#"><img src="images/weekly_1.jpg" alt="top_weekly" />
+          <a href="#"><img src="/static/images/weekly_1.jpg" alt="top_weekly" />
           <span class="time">2:47</span>
           <span class="overlay"></span></a>
         </figure>
@@ -288,7 +188,7 @@
       </li>
       <li>
         <figure class="pic">
-          <a href="#"><img src="images/weekly_1.jpg" alt="top_weekly" />
+          <a href="#"><img src="/static/images/weekly_1.jpg" alt="top_weekly" />
           <span class="time">2:47</span>
           <span class="overlay"></span></a>
         </figure>
@@ -304,27 +204,3 @@
     <!-- Top Weekly Download [e] -->
 
     </section>
-
-    <section id="engagement" class="for-mobile">
-      <h3>Now start using SnapTube</h3>
-      <p><a href="http://dl-master.snappea.com/installer/snaptube/latest/Click_me_to_install_SnapTube_tube_homepage.apk" onclick="ga('send', 'event', 'snaptubevideo.com', 'snaptube', 'download');" class="btn">Download</a></p>
-    </section>
-    <footer>
-      <ul>
-        <li class="copyright">Copyright &copy; 2014 SnapTube</li>
-        <li><a href="//www.snaptubevideo.com/contact">Contact</a></li>
-        <li><a href="//www.snaptubevideo.com/terms">Terms</a></li>
-        <li><a href="//www.snaptubevideo.com/privacy">Privacy</a></li>
-      </ul>
-    </footer>
-    <script>
-      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-      
-      ga('create', 'UA-56844332-1', 'auto');
-      ga('send', 'pageview');
-    </script>
-  </body>
-</html>
